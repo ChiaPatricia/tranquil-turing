178aa11d0a92f3e4e6f7270d5d0ef7e3bf744889
Andrew Nanton <nanton@thealienists.org>
2025-01-27 16:16:27 -0800

    Trivial working example of functional gemini script in andy-scratch.py

--- andy-scratch.py
+++ andy-scratch.py
@@ -1,11 +1,11 @@
+import os
 from google import genai
 from google.genai import types
 
 from dotenv import load_dotenv
 load_dotenv()
 
-# Only run this block for Gemini Developer API
-client = genai.Client(api_key="GEMINI_API_KEY")
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 response = client.models.generate_content(
     model="gemini-2.0-flash-thinking-exp-01-21",
